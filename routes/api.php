01a7701818e9a4915656000e2105948480169fc7
Charbel Elia <charbelelia05@gmail.com>
2025-10-01 11:17:35 +0300

    changed the apis and created controllers and requests

--- routes/api.php
+++ routes/api.php
@@ -3,16 +3,20 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
-// ADD THIS ROUTE
+use App\Http\Controllers\CourseController;
+
+// Auth Routes
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
+
 Route::middleware('auth:sanctum')->group(function () {
+    // User routes
     Route::get('/profile', function (Request $request) {
         return response()->json($request->user());
     });
-
     Route::post('/logout', [AuthController::class, 'logout']);
-});
-Route::get('/ping', function () {
-    return response()->json(['message' => 'pong']);
+
+    Route::apiResource('courses', CourseController::class);
+    Route::apiResource('tracks', TrackController::class);
+    Route::apiResource('course-progress', CourseProgressController::class);
 });
